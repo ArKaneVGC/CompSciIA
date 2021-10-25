05895a03b09def649f388bea6722e55059db077c
RKArKane <73201654+RKArKane@users.noreply.github.com>
2021-10-24 21:42:30 -0400

    Initial Commit

---
+++ src/com/danielkyu2004/ibslia/Objects/Window.java
@@ -0,0 +1,20 @@
+package com.danielkyu2004.ibslia.Objects;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window {
+    public JFrame frame;
+
+    public Window(){
+        frame=new JFrame();
+        frame.setSize(new Dimension(500,300));
+
+
+
+        frame.setVisible(true);
+        frame.setSize(new Dimension(300,500));
+
+        frame.dispose();
+    }
+}
